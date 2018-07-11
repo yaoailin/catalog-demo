2f33d947b80bc1245b12027831bdb495d576e2f6
yaoailin <yaoailin@gomeholdings.com>
2018-07-11 14:22:44 +0800

    fx

---
+++ selenium-hub-chrome/.pm
@@ -0,0 +1,20 @@
+package hello;
+
+# This module has been generated using Parse::Eyapp::Treereg
+# from file hello-srv/. Don't modify it.
+# Change hello-srv/ instead.
+# Copyright (c) Casiano Rodriguez-Leon 2006. Universidad de La Laguna.
+# You may use it and distribute it under the terms of either
+# the GNU General Public License or the Artistic License,
+# as specified in the Perl README file.
+
+use strict;
+use warnings;
+use Carp;
+use Parse::Eyapp::_TreeregexpSupport qw(until_first_match checknumchildren);
+
+our @all = ( ) = Parse::Eyapp::YATW->buildpatterns();
+
+
+1;
+
